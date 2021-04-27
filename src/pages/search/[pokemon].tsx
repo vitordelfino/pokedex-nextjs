9957e1cddb11b5f7990f7524bcd7b7062835d276
Vitor Delfino <vitor.delfino952@gmail.com>
2021-04-27 15:24:02 -0300

    feat: search page

--- src/pages/search/[pokemon].tsx
+++ src/pages/search/[pokemon].tsx
@@ -8,13 +8,13 @@ import {
   Text,
   HStack,
   Wrap,
-  WrapItem
+  WrapItem,
+  Spinner,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import PokemonCard from "../../components/PokemonCard";
 import { useRouter } from "next/router";
 import { useSearchPokemon } from "../../hooks/useSearchPokemon";
-
 
 const Search = () => {
   const MotionCenter = motion(Center);
@@ -31,42 +31,44 @@ const Search = () => {
       }}
     >
       <VStack spacing={10} w="100%">
-        <Stack direction="row" w="100%" justifyContent="center">
-          <PokemonCard
-            name={pokemon as string}
-            url=""
-            cursor="auto"
-            hoverless
-          />
-          <Box p={8} bg="gray.800" borderRadius="md" mt={5} w="md">
-            <Stack spacing={8}>
-              <HStack>
-                <FormControl>
-                  <FormLabel fontSize="xl">Height</FormLabel>
-                  <Text>{data.height}</Text>
-                </FormControl>
+        {isLoading && <Spinner />}
+        {data && (
+          <Stack direction="row" w="100%" justifyContent="center">
+            <PokemonCard
+              name={pokemon as string}
+              url=""
+              cursor="auto"
+              hoverless
+            />
+            <Box p={8} bg="gray.800" borderRadius="md" mt={5} w="md">
+              <Stack spacing={8}>
+                <HStack>
+                  <FormControl>
+                    <FormLabel fontSize="xl">Height</FormLabel>
+                    <Text>{data.height}</Text>
+                  </FormControl>
 
-                <FormControl>
-                  <FormLabel fontSize="xl">Weight</FormLabel>
-                  <Text>{data.weight}</Text>
-                </FormControl>
-              </HStack>
-              <HStack>
-                <Wrap>
-
-                  {data.stats.map((s: any) => (
-                    <WrapItem>
-                      <FormControl>
-                        <FormLabel fontSize="md">{s.stat.name}</FormLabel>
-                        <Text>{s.base_stat}</Text>
-                      </FormControl>
-                    </WrapItem>
-                  ))}
-                </Wrap>
-              </HStack>
-            </Stack>
-          </Box>
-        </Stack>
+                  <FormControl>
+                    <FormLabel fontSize="xl">Weight</FormLabel>
+                    <Text>{data.weight}</Text>
+                  </FormControl>
+                </HStack>
+                <HStack>
+                  <Wrap>
+                    {data.stats.map((s: any) => (
+                      <WrapItem>
+                        <FormControl>
+                          <FormLabel fontSize="md">{s.stat.name}</FormLabel>
+                          <Text>{s.base_stat}</Text>
+                        </FormControl>
+                      </WrapItem>
+                    ))}
+                  </Wrap>
+                </HStack>
+              </Stack>
+            </Box>
+          </Stack>
+        )}
       </VStack>
     </MotionCenter>
   );
